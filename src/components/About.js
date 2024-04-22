ce827f7b678d3a690602ea8f28ec0858d298e268
legojerry <38884404+legojerry@users.noreply.github.com>
2024-04-21 21:58:37 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
         {/* Mint Shortcode */}
         <div className="fn_cs_mint">
           <div className="left_part">
-            <h3 className="fn__maintitle" data-text="How to Mint">
-              How to Mint
+            <h3 className="fn__maintitle" data-text="Learn how to unlock GATEs.">
+            Learn how to unlock GATEs.
             </h3>
             <div className="fn_cs_divider">
               <div className="divider">
@@ -18,37 +18,15 @@ const About = () => {
             </div>
             <div className="desc">
               <p>
-                As the first hero of the Meta Legends, collection has over 9,999
-                unique skins drawn from the different missions and challenges he
-                faced throughout his life.
+              The Creators is a discovery platform with gamification of creative drops, limited events, and exclusive content. Unlock Gates that lead to this content by solving puzzles, finding secrets, and participating in community engagement initiatives. By meeting onchain, social, and identity-based requirements, you'll gain  👁️  points, GATEs, & SOUL - which measures your unique achievements, proving your skill level on the global leaderboard.
               </p>
               <p>
-                The artwork has been hand-drawned by Robert Green in the
-                traditional anime style and composited by Layla Efiyo.
-              </p>
-              <p>
-                Curabitur pharetra velit eget dignissim varius. In vulputate
-                elit at tortor pellentesque, non pulvinar neque consequat.
-                Aenean tristique odio in libero tincidunt maximus. Nulla
-                pharetra viverra dolor ut blandit. Cras finibus vel tortor eget
-                lacinia. Pellentesque interdum elit non lacinia faucibus. Morbi
-                nec felis auctor, tincidunt lacus sit amet, iaculis ipsum.
-                Phasellus tempus sit amet justo et feugiat. Duis blandit semper
-                lorem, egestas euismod ligula pharetra ac. Sed porta lorem eget
-                neque bibendum, eget euismod justo mollis.
-              </p>
-              <p>
-                Donec tristique porttitor sem, eget fermentum elit varius nec.
-                Donec ut orci ipsum. Morbi efficitur felis eget dapibus
-                fermentum. Phasellus sed tellus volutpat, hendrerit leo non,
-                sollicitudin neque. Etiam ac lacus quam. Vivamus suscipit nisl
-                tellus, at congue odio commodo at. Cras ante enim, sodales at
-                pretium et, tempus at libero.
+              For our MVP alpha test, players unlock achievements by expanding their web3 footprint, finding onchain secrets, such as owning certain NFTs, holding certain levels of DEGEN token. We hope you'll enjoy our MVP alpha test. If you give us feedback below, you'll earn points on our platform for future drops:
               </p>
             </div>
-            <Link href="">
+            <Link href="https://warpcast.com/rskagy.eth/0x5acbbd99">
               <a className="metaportal_fn_button">
-                <span>How to Mint</span>
+                <span>Provide Feedback</span>
               </a>
             </Link>
           </div>
@@ -60,7 +38,7 @@ const About = () => {
                   <div className="item">
                     <div className="item_in">
                       <h3 className="fn__gradient_title">01</h3>
-                      <p>Connect your Wallet</p>
+                      <p>Tap a locked GATE</p>
                     </div>
                   </div>
                 </li>
@@ -68,7 +46,7 @@ const About = () => {
                   <div className="item">
                     <div className="item_in">
                       <h3 className="fn__gradient_title">02</h3>
-                      <p>Select Your Quantity</p>
+                      <p>See the requirements for unlock</p>
                     </div>
                   </div>
                 </li>
@@ -76,7 +54,7 @@ const About = () => {
                   <div className="item">
                     <div className="item_in">
                       <h3 className="fn__gradient_title">03</h3>
-                      <p>Confirm The Transaction</p>
+                      <p>Meet the requirements</p>
                     </div>
                   </div>
                 </li>
@@ -84,7 +62,7 @@ const About = () => {
                   <div className="item">
                     <div className="item_in">
                       <h3 className="fn__gradient_title">04</h3>
-                      <p>Receive Your NFT’s</p>
+                      <p>Unlock the GATE</p>
                     </div>
                   </div>
                 </li>
